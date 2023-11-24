0ab2afc3104dedcd2715d0f20644f13c10d3372e
Kamran <axmedovkamran19@gmail.com>
2023-11-24 15:33:12 +0400

    contact added

---
+++ src/main/java/az/crocusoft/ecommerce/exception/MailSenderException.java
@@ -0,0 +1,8 @@
+package az.crocusoft.ecommerce.exception;
+
+public class MailSenderException extends RuntimeException{
+
+    public MailSenderException(String message){
+        super(message);
+    }
+}
--- src/main/java/az/crocusoft/ecommerce/exception/test.java
+++
@@ -1,4 +0,0 @@
-package az.crocusoft.ecommerce.exception;
-
-public class test {
-}
