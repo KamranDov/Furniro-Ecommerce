7bb9eb7fcdcbfde484616d70767aa28afa1d48b2
OzlamHasanova <7qk9bme@code.edu.az>
2023-12-06 12:22:05 +0400

    last update

---
+++ src/main/java/az/crocusoft/ecommerce/exception/WishListNotFoundException.java
@@ -0,0 +1,6 @@
+package az.crocusoft.ecommerce.exception;
+
+public class WishListNotFoundException extends RuntimeException{
+    public WishListNotFoundException(String message) {
+        super(message);
+    }}
