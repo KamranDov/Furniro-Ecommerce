55b15c8f5b21646fc0b278d0cb6a3f2a1fe911fa
Rehman Eliyev <125017696+R3HM4N@users.noreply.github.com>
2023-12-08 02:02:38 -0800

    webconfig update

--- src/main/java/az/crocusoft/ecommerce/config/WebConfig.java
+++ src/main/java/az/crocusoft/ecommerce/config/WebConfig.java
@@ -2,6 +2,7 @@ package az.crocusoft.ecommerce.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:" + uploadDir + "/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
     }
 }
