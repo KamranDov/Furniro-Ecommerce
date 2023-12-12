a3220688e4ed866ac620f8e4742b95ac09d10009
Kamran <axmedovkamran19@gmail.com>
2023-12-12 12:20:04 +0400

    scheduler added

--- src/main/java/az/crocusoft/ecommerce/repository/CartRepository.java
+++ src/main/java/az/crocusoft/ecommerce/repository/CartRepository.java
@@ -2,17 +2,20 @@ package az.crocusoft.ecommerce.repository;
 
 import az.crocusoft.ecommerce.model.Cart;
 import az.crocusoft.ecommerce.model.User;
-import org.mapstruct.control.MappingControl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     List<Cart> findAllByUserOrderByCreatedDateDesc(User user);
+
     Cart findByUser(User user);
+
+    List<Cart> findCartByCreatedDateBefore(LocalDate localDate);
 }
 
 
