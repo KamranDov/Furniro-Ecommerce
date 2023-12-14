18a68e8c01ac82e2bb20f97b236ca84015f32e10
Rehman Eliyev <125017696+R3HM4N@users.noreply.github.com>
2023-12-14 10:47:57 +0400

    Update OrderServiceImpl.java

--- src/main/java/az/crocusoft/ecommerce/service/impl/OrderServiceImpl.java
+++ src/main/java/az/crocusoft/ecommerce/service/impl/OrderServiceImpl.java
@@ -86,6 +86,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void deleteOrder(Long orderId) {
+        orderItemRepository.deleteById(orderId);
         orderRepository.deleteById(orderId);
     }
 
