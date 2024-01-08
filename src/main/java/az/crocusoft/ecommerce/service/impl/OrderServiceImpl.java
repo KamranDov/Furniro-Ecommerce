7d4bd61b3757609cb9e859dbb5c1788d9c234820
OzlamHasanova <7qk9bme@code.edu.az>
2024-01-08 15:21:51 +0400

    update order

--- src/main/java/az/crocusoft/ecommerce/service/impl/OrderServiceImpl.java
+++ src/main/java/az/crocusoft/ecommerce/service/impl/OrderServiceImpl.java
@@ -127,7 +127,7 @@ public class OrderServiceImpl implements OrderService {
     public List<OrderDto> getOrdersByUserId(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-        List<Order> userOrders = user.getOrders();
+        List<Order> userOrders = orderRepository.findAllByUserId(userId);
         return userOrders.stream()
                 .map(order -> modelMapper.map(order, OrderDto.class))
                 .collect(Collectors.toList());
